e8a422455e54f88319103290683a2540e00f6250
James <60515492+jamescortezzzzzzz@users.noreply.github.com>
2023-03-20 02:01:14 -0400

    errors with FinRL library...

--- youtubeVidCode.py
+++ youtubeVidCode.py
@@ -31,3 +31,21 @@ import sys
 sys.path.append("../FinRL-Library")
 
 import itertools
+
+import os
+from finrl.main import check_and_make_directories
+from finrl.config import (
+    DATA_SAVE_DIR,
+    TRAINED_MODEL_DIR,
+    TENSORBOARD_LOG_DIR,
+    RESULTS_DIR,
+    INDICATORS,
+    TRAIN_START_DATE,
+    TRAIN_END_DATE,
+    TEST_START_DATE,
+    TEST_END_DATE,
+    TRADE_START_DATE,
+    TRADE_END_DATE,
+)
+
+check_and_make_directories([DATA_SAVE_DIR, TRAINED_MODEL_DIR, TENSORBOARD_LOG_DIR, RESULTS_DIR])
